f3b09fceab6fdd8bb9c3a9755901e2cffcbec1dc
MG <MG_Laptop@ip2088.saw.rwth-aachen.de>
2015-04-16 23:27:40 +0200

    Hoffentlich klappts

--- myecho.c
+++ myecho.c
@@ -2,7 +2,14 @@
 
 int main(int argc, char* argv[], char* envp[]) 
 {
-  printf("Dies ist eine tolle Lösung\n");
+
+	// Ausgabe aller Paramater, auf die argv zeigt
+
+	for (int i = 1; i < argc; i++)
+	{
+		printf("Parameter Nr. %d: %s", i, argv[i]);
+	}
+
 
   return 0;
 }

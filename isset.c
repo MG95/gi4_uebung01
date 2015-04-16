16309594431469b1ca2733cedd44a66fda3b14c6
MG <MG_Laptop@ip2088.saw.rwth-aachen.de>
2015-04-16 23:38:34 +0200

    neu

--- isset.c
+++ isset.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[], char* envp[])
 
 	// PrŸfe, ob Umgebungsvariable gesetzt ist und gib entsprechende Ausgabe aus
 	
-	if (argc >= 3 && *(getenv(argv[1])) && strcmp(argv[2], "-v")
+	if (argc >= 3 && *(getenv(argv[1])) && strcmp(argv[2], "-v"))
 	{
 		printf("%s", getenv(argv[1]));
 		return 0;
